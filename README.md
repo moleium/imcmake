27cfa598bf580a0dda6e8e4bd03d81413cd130bf
MoleSchizo <molel768@gmail.com>
2023-08-19 10:43:29 +0300

    i like femboys

---
+++ README.md
@@ -0,0 +1,20 @@
+# imcmake
+
+CMake project for ImGui
+
+## Requirements
+- A C++ compiler that supports C++11
+
+## Building
+
+To build the porject, follow these:
+
+1. Clone this repo
+2. Create a `build` directory inside the repository and navigate to it
+3. Run `cmake ..` to generate the build files, for example `cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ ../`.
+4. Run `make` to build it 
+
+After building the project you should have an executable (or a solution, etc..) in the current directory
+
+## License 
+This project is licensed under the MIT License.
---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+  GLFWwindow* window;
+
+  glfwInit();
+  window = glfwCreateWindow(1280, 720, ">w<", NULL, NULL);
+  glfwMakeContextCurrent(window);
+
+  ImGui::CreateContext();
+
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 130");
+  glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+
+  bool dirty = true;
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+
+            if (ImGui::IsMousePosValid())
+                dirty = true;
+
+            if (!dirty)
+                continue;
+
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+
+            ImGui::NewFrame();
+
+            ImGui::SetNextWindowPos(ImVec2(0, 0));
+            ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+            ImGui::Begin("Background", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoBringToFrontOnFocus);
+
+            // rendering goes here
+            //
+
+            ImGui::End();
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glfwSwapBuffers(window);
+        }
+
+  return 0;
+}
